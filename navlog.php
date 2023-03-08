eb0690b7b111e72906d414e59f283fdf5180b430
Diego Lemos <diegolemosgarcia@gmail.com>
2023-03-08 18:10:03 +0100

    creo paginas inises y register

--- navlog.php
+++ navlog.php
@@ -1,9 +1,9 @@
 <nav>
-    <img src="img/favi.jpg" href="login.php" alt="logo">
+    <a href="login.php"><img src="img/favi.jpg" alt="logo"></a>
     <div class="menu">
         <ul class="ulnav">
-            <li class="linav"><a href="">Iniciar Sesion</a></li>
-            <li class="linav"><a href="">Registrarse</a></li>
+            <li class="linav"><a href="inises.php">Iniciar Sesion</a></li>
+            <li class="linav"><a href="register.php">Registrarse</a></li>
         </ul>
     
         <label id="id"><input class="input" onclick="darkmode()" type="checkbox"><div class="magia"></div></label>
---
+++ register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro de usuario</title>
+    <link href="css/loginestilo.css" rel="stylesheet" type="text/css" media="screen"/>
+    <link rel="icon" type="image/jpg" href="img/favi.jpg" />
+</head>
+<body>
+    <?php
+        include "navlog.php";
+    ?>
+</body>
+</html>
