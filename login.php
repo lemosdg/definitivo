d48b2099b9f07c7d70fcdb4b94f1d022d315671b
Diego Lemos <diegolemosgarcia@gmail.com>
2023-03-08 18:02:50 +0100

    navlog include

--- login.php
+++ login.php
@@ -6,17 +6,8 @@
     <link rel="icon" type="image/jpg" href="img/favi.jpg" />
 </head>
 <body>
-    <nav>
-        <img src="img/favi.jpg" alt="logo">
-        <div class="menu">
-            <ul class="ulnav">
-                <li class="linav1"><img class="resp" src="imagenes/edit.svg" onclick="menu()"></li>
-                <li class="linav"><a href="">Iniciar Sesion</a></li>
-                <li class="linav"><a href="">Registrarse</a></li>
-            </ul>
-        
-            <label id="id"><input class="input" onclick="darkmode()" type="checkbox"><div class="magia"></div></label>
-        </div>
-    </nav>
+    <?php
+        include "navlog.php";
+    ?>
 </body>
 </html>
---
+++ navlog.php
@@ -0,0 +1,11 @@
+<nav>
+    <img src="img/favi.jpg" href="login.php" alt="logo">
+    <div class="menu">
+        <ul class="ulnav">
+            <li class="linav"><a href="">Iniciar Sesion</a></li>
+            <li class="linav"><a href="">Registrarse</a></li>
+        </ul>
+    
+        <label id="id"><input class="input" onclick="darkmode()" type="checkbox"><div class="magia"></div></label>
+    </div>
+</nav>
