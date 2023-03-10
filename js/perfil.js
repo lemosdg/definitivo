f2db52f4aab6074c80bdea05f6bdf754d6d56b6a
Diego Lemos <diegolemosgarcia@gmail.com>
2023-03-10 11:23:15 +0100

    comento codigo

--- js/perfil.js
+++ js/perfil.js
@@ -1,3 +1,5 @@
+// funcion log out
+
 function confirmarLogout() {
     if (confirm("¿Está seguro de que desea cerrar sesion?")) {
       // Acción a realizar si el usuario confirma el borrado
--- navperfil.php
+++ navperfil.php
@@ -1,13 +1,16 @@
+<!-- barra de navegación del perfil -->
 <nav>
+    <!-- logos -->
     <div class="imgnav">
         <img class="logonav" src="img/favi.jpg" alt="logo">
         <a class="logout" onclick="confirmarLogout()"><img class="logoutnav" src="img/logout.png" alt="logout"></a>
     </div>
+    <!-- menu -->
     <ul class="ulnav">
         <li class="linav"><a href="inicio.php">Inicio</a></li>
         <li class="linav"><a href="">Perfil</a></li>
         <li class="linav"><a href="">Social</a></li>
     </ul>
-
+    <!-- darkmode -->
     <label class="dark"><input class="input" onclick="darkmode()" type="checkbox"><div class="magia"></div></label>
 </nav>
