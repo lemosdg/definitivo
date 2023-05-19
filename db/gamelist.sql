be3e4de1f41a473d2f4104ba30c91351e498adac
lemosdg <75094524+lemosdg@users.noreply.github.com>
2023-05-19 16:05:53 +0200

    Update gamelist.sql
    insert para la expo

--- db/gamelist.sql
+++ db/gamelist.sql
@@ -48,3 +48,7 @@ CREATE TABLE juego_has_usuario (
 );
 
 INSERT INTO rol_usuario VALUES (1,'admin'),(2,'mod'),(3,'pleb');
+INSERT INTO juego (Nombre, FechaLanzamiento, Desarrolladora, Descripcion, Saga, ImgJuego) VALUES ('Dark Souls 3','2013-05-19','From Software','souls like', 'Dark Souls', 'https://images.greenmangaming.com/4f2c7d419e0b41868dfac773a5f91b50/ab7a542f485642c1bada1714091d43bb.jpg');
+INSERT INTO juego (Nombre, FechaLanzamiento, Desarrolladora, Descripcion, Saga, ImgJuego) VALUES ('The last of us','2013-05-19','no se','apocalipsis', 'The last of us', 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2012/12/200011-last-us-su-portada-multijugador-incentivos.jpg?tf=3840x');
+INSERT INTO juego (Nombre, FechaLanzamiento, Desarrolladora, Descripcion, Saga, ImgJuego) VALUES ('The last of us 2','2013-05-19','no se','apocalipsis', 'The last of us', 'https://i.blogs.es/2696f8/tlou2/1366_2000.jpeg');
+INSERT INTO juego (Nombre, FechaLanzamiento, Desarrolladora, Descripcion, Saga, ImgJuego) VALUES ('Far cry 3','2013-05-19','Ubisoft','survi', 'Far cry', 'https://gaming-cdn.com/images/products/96/616x353/juego-uplay-far-cry-3-cover.jpg?v=1625493046');
